e2bee4aa3ab67ecbbdd994f807ed3b4b18249cf1
charlescharles <platypode@gmail.com>
2015-01-21 02:10:12 -0500

    more shit

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,17 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main where
 
-import           Data.Bits              (xor)
+import           Control.Arrow          (first)
+import           Data.Bits              (xor, popCount)
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8  as C8
 import           Data.Char              (ord)
 import           Data.List              (sortBy)
+import qualified Data.Map               as M
+import qualified Data.Set               as S
 import qualified Data.Word8             as W8
 
 -- Set 1
@@ -31,16 +36,6 @@ fromHex = fst . B16.decode . C8.pack
 xorSingle :: W8.Word8 -> BS.ByteString -> BS.ByteString
 xorSingle w = BS.map (xor w)
 
-scoreByChar :: (W8.Word8 -> Int) -> BS.ByteString -> Int
-scoreByChar f = sum . map f . BS.unpack
-
-englishScore :: W8.Word8 -> Int
-englishScore = fromEnum . (`elem` english) where
-    english = map charToWord8 [' '..'z']
-
-alphaScore :: W8.Word8 -> Int
-alphaScore c = fromEnum (W8.isAscii c && W8.isAlpha c)
-
 rankBy :: (BS.ByteString -> Int) -> [BS.ByteString] -> [BS.ByteString]
 rankBy score = reverse . sortBy f where
 		f a b = compare (score a) (score b)
@@ -48,15 +43,43 @@ rankBy score = reverse . sortBy f where
 possibilities :: BS.ByteString -> [BS.ByteString]
 possibilities s = map (`xorSingle` s) [minBound..maxBound]
 
-score3 :: W8.Word8 -> Int
-score3 c = englishScore c + alphaScore c
-
 decodeXor :: String -> String
-decodeXor = C8.unpack . head . rankBy score . possibilities . fromHex where
-    score = scoreByChar score3
+--decodeXor = C8.unpack . head . rankBy score . possibilities . fromHex
+decodeXor !s = (C8.unpack . head . rankBy score . possibilities . fromHex) s
 
 -- Ch 4
---decode4 :: [String] -> String
+englishFrequencies' :: [(Char, Float)]
+englishFrequencies' = [ (' ', 0.183)
+                     , ('e', 0.103)
+                     , ('t', 0.075)
+                     , ('a', 0.065)
+                     , ('o', 0.062)
+                     , ('n', 0.057)
+                     , ('i', 0.057)
+                     , ('s', 0.053)
+                     , ('r', 0.050)
+                     , ('h', 0.050)
+                     , ('l', 0.033) ]
+
+englishFrequencies :: M.Map W8.Word8 Float
+englishFrequencies = M.fromList $ map (first (fromIntegral . ord)) englishFrequencies'
+
+score :: BS.ByteString -> Int
+score s = {-# SCC score #-} M.foldrWithKey f 0 fr where
+    n = BS.length s
+    fr = freqs (BS.map W8.toLower s)
+    f ch ct acc = case M.lookup ch englishFrequencies of
+                    Nothing -> -ct + acc
+                    Just frac -> -(absDiff frac ct) + acc
+    absDiff :: Float -> Int -> Int
+    absDiff p ct = abs $ round (p * (fromIntegral n)) - ct
+
+freqs :: BS.ByteString -> M.Map W8.Word8 Int
+freqs = BS.foldr' f M.empty where
+    f w = M.insertWith' (+) w 1
+
+decodeAll :: [String] -> String
+decodeAll = {-# SCC decodeAll #-} C8.unpack . head . rankBy score . map (C8.pack . decodeXor)
 
 -- Ch 5
 xorEncrypt :: String -> String -> String
@@ -64,17 +87,35 @@ xorEncrypt text k = (C8.unpack . B16.encode . BS.pack) (BS.zipWith xor bsText (r
     bsText = C8.pack text
     rkey = C8.pack $ take (length text) (concat $ repeat k)
 
+-- Ch 6
+dist :: BS.ByteString -> BS.ByteString -> Int
+dist = sum . map popCount . BS.zipWith xor
+
+first2chunks :: Int -> BS.ByteString -> (BS.ByteString, BS.ByteString)
+first2chunks n s = (a, b) where
+    (a, rest) = splitAt n s
+    b = take n rest
+
+-- bytestring is the total text, int is keysize, float is
+-- (hamming dist between first/second chunk of n bytes)/keysize
+normalized2delta :: BS.ByteString -> Int -> Float
+normalized2delta s n = (dist a b) / (fromIntegral n) where
+    (a, b) = first2chunks n
 
 -- all challenges
-set1 :: [String]
-set1 = [ hexToBase64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
-          xorHex "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965",
-          decodeXor "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736" ]
+set1 :: [IO String]
+set1 = [ return $ hexToBase64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d",
+          return $ xorHex "1c0111001f010100061a024b53535009181c" "686974207468652062756c6c277320657965",
+          return $ decodeXor "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736",
+--         readFile "src/s1c4.txt" >>= return . decodeAll . lines,
+         return $ xorEncrypt "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal" "ICE"]
+
 
 main :: IO ()
 main = do
   let display (i, s) = putStrLn ("\tChallenge " ++ show i ++ ":\n\t>>" ++ s ++ "\n")
   putStrLn "Set 1:"
-  mapM_ display (zip [1..] set1)
+  challenges <- sequence set1
+  mapM_ display (zip [1..] challenges)
 
 
